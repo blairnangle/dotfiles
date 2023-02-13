83375322529edfca73e5efb6b36d9223ae99cbf2
Blair Nangle <blair@surgeryhero.com>
2023-02-13 17:25:15 +0000

    Spring-clean: big rewrite for blog post

--- README.md
+++ README.md
@@ -1,22 +1,14 @@
 # dotfiles
 
-Source control for my dotfiles.
+Source control for my dotfiles!
+
+I wrote a [blog post](https://blairnangle.com/blog/dotfiles) explaining some of my choices.
 
 ## Setup
 
-After cloning the project, you will need to create a symlink between your `.zshrc` file and your source-controlled,
-newly-downloaded `zsh_profile`:
-
-```bash
-$ ln -s dotfiles/zsh_profile ~/.zshrc
+```shell
+./set-up.sh
 ```
-
-`zsh_profile` will source the files it needs to, so this one symlink should be enough.
-
-### Hardcoded paths
-
-Until the absolute paths in `zsh_profile` are replaced with relative ones, the paths will need to be changed after
-setting up a new machine.
 
 ## License
 
---
+++ set-up.sh
@@ -0,0 +1,27 @@
+#!/bin/zsh
+
+set -e -o pipefail
+
+echo "Installing fonts…"
+brew tap homebrew/cask-fonts
+brew install --cask font-jetbrains-mono-nerd-font
+osascript -e "tell application \"Terminal\" to set the font name of window 1 to \"JetBrainsMono Nerd Font\""
+
+echo "Configuring zsh…"
+if [ -f ~/.zshrc ]; then
+  printf "\nExisting .zshrc found. Quitting auto set up."
+  exit 1
+else
+  echo "Sym-linking .zshrc…"
+  ln -s dotfiles/zsh_profile ~/.zshrc
+fi
+
+echo "Installing and configuring starship…"
+brew install starship
+if [ -f ~/.config/starship.toml ]; then
+  printf "\nExisting starship.toml found. Quitting auto set up."
+  exit 1
+else
+  echo "Sym-linking starship.toml…"
+  ln -s ~/dotfiles/starship.toml ~/.config/starship.toml
+fi
